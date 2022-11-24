f99aef994947e51d1a28cbd8f5979096e85c1e06
jude <zpd106@sina.com>
2022-11-24 11:41:34 +0800

    fix: add index

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,411 @@
-import { NativeModules, Platform } from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  EmitterSubscription,
+  AppState,
+  AppStateStatus,
+} from 'react-native';
+import { useEffect, useState, useRef } from 'react';
 
-const LINKING_ERROR =
-  `The package 'react-native-esp-idf' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+export type EspProvisioning = {
+  /**
+   * check if has proper permissions, if not will request the needed permissions at native
+   */
+  checkPermissions(): Promise<boolean>;
+  /**
+   * search BLE ESP device
+   * @param prefix prefix of device name
+   */
+  startBleScan(prefix: string | null): Promise<boolean>;
+  stopBleScan(): void;
+  /**
+   * connect to an ESP device
+   * @param uuid device serviceID
+   * @param pop proof of possession
+   */
+  connectDevice(uuid: string, pop: string | null): Promise<boolean>;
+  /**
+   * connet to an ESP device through wifi
+   * @param pop proof of possession
+   */
+  connectWifiDevice(pop: string | null): Promise<boolean>;
+  /**
+   * disconnect the connected ESP device
+   */
+  disconnectDevice(): void;
+  /**
+   *
+   */
+  startWifiScan(): Promise<boolean>;
+  /**
+   * provisioning Wi-Fi configuration
+   * @param ssidValue Wi-Fi SSID
+   * @param passphraseValue password
+   */
+  doProvisioning(ssidValue: string, passphraseValue: string): Promise<boolean>;
+};
 
-const EspIdf = NativeModules.EspIdf
-  ? NativeModules.EspIdf
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
+declare enum BleScanStatus {
+  FAILED = 0,
+  COMPLETED = 1,
+}
+export interface BleScanEvent {
+  status: BleScanStatus;
+}
+export interface BleDevice {
+  deviceName: string;
+  serviceUuid: string;
+}
+
+type BleScanEventListener = (
+  event: BleScanEvent | BleDevice | BleDevice[]
+) => void;
+
+export interface EspEventEmitter extends NativeEventEmitter {
+  addListener(
+    eventType: 'scanBle',
+    listener: BleScanEventListener
+  ): EmitterSubscription;
+}
+
+declare enum DeviceConnectionStatus {
+  CONNECTED = 1,
+  FAILED = 2,
+  DISCONNECTED = 3,
+}
+export interface DeviceConnectionEvent {
+  status: DeviceConnectionStatus;
+}
+type DeviceConnectionEventListener = (event: DeviceConnectionEvent) => void;
+export interface EspEventEmitter extends NativeEventEmitter {
+  addListener(
+    eventType: 'connection',
+    listener: DeviceConnectionEventListener
+  ): EmitterSubscription;
+}
+
+declare enum WifiScanStatus {
+  FAILED = 0,
+}
+declare enum WifiAuthMode {
+  WIFI_UNKNOWN = -1,
+  WIFI_OPEN = 0,
+  WIFI_WEP = 1,
+  WIFI_WPA_PSK = 2,
+  WIFI_WPA2_PSK = 3,
+  WIFI_WPA_WPA2_PSK = 4,
+  WIFI_WPA2_ENTERPRISE = 5,
+}
+export interface WifiAP {
+  ssid: string;
+  auth: WifiAuthMode;
+  rssi: number;
+}
+export type WifiAPWithPwd = WifiAP & { password: string };
+export interface WifiScanEvent {
+  status?: WifiScanStatus;
+  message?: string;
+  wifiList?: WifiAP[];
+}
+type WifiScanEventListener = (event: WifiScanEvent) => void;
+export interface EspEventEmitter extends NativeEventEmitter {
+  addListener(
+    eventType: 'scanWifi',
+    listener: WifiScanEventListener
+  ): EmitterSubscription;
+}
+
+declare enum PermissionType {
+  REQUEST_ENABLE_BT = 1,
+  REQUEST_FINE_LOCATION = 2,
+}
+
+declare enum PermissionStatus {
+  UNKNOWN = 0,
+  LIMITED = 1,
+  DENIED = 2,
+  ALLOWED = 3,
+}
+export interface PermissionEvent {
+  type: PermissionType;
+  status: PermissionStatus;
+}
+type PermissionEventListener = (event: PermissionEvent) => void;
+export interface EspEventEmitter extends NativeEventEmitter {
+  addListener(
+    eventType: 'permission',
+    listener: PermissionEventListener
+  ): EmitterSubscription;
+}
+
+declare enum ProvisioningStatus {
+  PROV_INIT_FAILED = 0,
+  PROV_CONFIG_SENT = 1,
+  PROV_CONFIG_FAILED = 2,
+  PROV_CONFIG_APPLIED = 3,
+  PROV_APPLY_FAILED = 4,
+  PROV_COMPLETED = 5,
+  PROV_FAILED = 6,
+}
+export interface ProvisioningEvent {
+  status: ProvisioningStatus;
+  message?: string;
+}
+
+type ProvisioningEventListener = (event: ProvisioningEvent) => void;
+export interface EspEventEmitter extends NativeEventEmitter {
+  addListener(
+    eventType: 'provisioning',
+    listener: ProvisioningEventListener
+  ): EmitterSubscription;
+}
+
+const { RNTEspIdf } = NativeModules as { RNTEspIdf: EspProvisioning };
+
+const eventEmitter: EspEventEmitter = new NativeEventEmitter(RNTEspIdf as any);
+
+export default RNTEspIdf;
+
+export type MessageInfo = {
+  scanBle: string;
+  scanWifi: string;
+  connectDevice: string;
+  sendingWifiCredential: string;
+  confirmWifiConnection: string;
+  enableBluetooth: string;
+  enableLocation: string;
+  scanBleFailed: string;
+  connectFailed: string;
+  disconnected: string;
+  initSessionError: string;
+  completed: string;
+  applyError: string;
+};
+
+type ProvisioningProps = {
+  devicePrefix: string | null;
+  pop?: string | null;
+  message: MessageInfo;
+};
+export function useProvisioning({
+  devicePrefix,
+  pop = null,
+  message,
+}: ProvisioningProps) {
+  console.log('Invoke func useProvisioning');
+  const msg = useRef<MessageInfo>(message);
+  const [bleDevices, setBleDevices] = useState<BleDevice[]>([]);
+  const [wifiAPs, setWifiAPs] = useState<WifiAP[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [status, setStatus] = useState(message.scanBle);
+  const isConnecting = useRef(false);
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const [provSent, setProvSent] = useState(
+    initStep(message.sendingWifiCredential, true)
+  );
+  const [provApplied, setProvApplied] = useState(
+    initStep(message.confirmWifiConnection)
+  );
+  const [provFinal, setProvFinal] = useState(initStep(''));
+
+  const currentWifi = useRef<WifiAP>();
+  const currentDevice = useRef<BleDevice>();
+
+  function connectDevice(bleDevice: BleDevice) {
+    if (isConnecting.current) return;
+    isConnecting.current = true;
+    RNTEspIdf.stopBleScan();
+    console.log('Connect to device:', bleDevice, pop);
+    setStatus(message.connectDevice);
+    currentDevice.current = bleDevice;
+    RNTEspIdf.connectDevice(bleDevice.serviceUuid, pop);
+  }
+
+  function configWifi(wifi: WifiAP) {
+    console.log('Selected Wifi:', wifi);
+    currentWifi.current = wifi;
+    setCurrentStep(!wifi.ssid || wifi.auth > 0 ? 3 : 4);
+    if (!wifi.auth) {
+      doProvisioning(wifi);
+    }
+  }
+
+  useEffect(() => {
+    eventEmitter.addListener('scanWifi', (event) => {
+      console.log('Event scanWifi', event);
+      setLoading(false);
+      setStatus('');
+      if (event.wifiList) {
+        setWifiAPs(event.wifiList);
+      } else if (event.message) {
+        setStatus(event.message);
       }
+    });
+    return function () {
+      console.log('Cleanup the resource');
+      RNTEspIdf.stopBleScan();
+      RNTEspIdf.disconnectDevice();
+      eventEmitter.removeAllListeners('scanWifi');
+    };
+  }, []);
+  useEffect(() => {
+    console.log('Added AppSateChanged listener');
+    async function _handleAppStateChange(nextAppState: AppStateStatus) {
+      console.log(`AppSateChanged: ${nextAppState}`);
+      if (nextAppState === 'active') {
+        console.log('Start checkPermissions');
+        const result = await RNTEspIdf.checkPermissions();
+        console.log(`checkPermissions result: ${result}`);
+        if (result && !isConnecting.current && currentStep === 1) {
+          console.log('Start BleScan');
+          RNTEspIdf.startBleScan(devicePrefix);
+        }
+      }
+    }
+    let eventAppState = AppState.addEventListener(
+      'change',
+      _handleAppStateChange
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return EspIdf.multiply(a, b);
+    eventEmitter.addListener('permission', (event) => {
+      console.log('Event permission', event);
+      if (event.status) {
+        if (currentStep === 1 && !isConnecting.current) {
+          RNTEspIdf.startBleScan(devicePrefix);
+          setBleDevices([]);
+        } else if (currentStep === 2) {
+          RNTEspIdf.startWifiScan();
+        }
+      } else {
+        setStatus(
+          event.type === 1
+            ? msg.current.enableBluetooth
+            : msg.current.enableLocation
+        );
+      }
+    });
+    return function () {
+      console.log('Removed AppSateChanged listener');
+      eventEmitter.removeAllListeners('permission');
+      eventAppState.remove();
+    };
+  }, [currentStep, devicePrefix]);
+
+  useEffect(() => {
+    console.log('Added listeners');
+    eventEmitter.addListener('scanBle', (event) => {
+      console.log('Event scanBle', event);
+      if (event instanceof Array) {
+        setBleDevices(event);
+      } else if ((event as BleDevice).deviceName) {
+        setBleDevices((prev) =>
+          prev.some((it) => it.serviceUuid === (event as BleDevice).serviceUuid)
+            ? prev
+            : prev.concat(event as BleDevice)
+        );
+      } else if ((event as BleScanEvent).status === 0) {
+        setLoading(false);
+        setStatus(msg.current.scanBleFailed);
+      } else {
+        setLoading(false);
+        if (!isConnecting.current) setStatus('');
+      }
+    });
+
+    eventEmitter.addListener('connection', (event) => {
+      isConnecting.current = false;
+      console.log('Event connection', event);
+      switch (event.status) {
+        case 1: //connected
+          RNTEspIdf.startWifiScan();
+          setCurrentStep(2);
+          setLoading(true);
+          setStatus(msg.current.scanWifi);
+          break;
+        case 2: //failed
+          setStatus(msg.current.connectFailed);
+          break;
+        case 3: //disconnected
+          setStatus(msg.current.disconnected);
+          break;
+      }
+    });
+
+    eventEmitter.addListener('provisioning', (event) => {
+      console.log('Event provisioning', event);
+      switch (event.status) {
+        case 0:
+        case 2:
+          setProvSent(doneStep(event.message!, true));
+          setProvFinal(doneStep(msg.current.initSessionError, true));
+          break;
+        case 3:
+          setProvSent((prev) => ({ ...prev, done: true }));
+          setProvApplied((prev) => ({ ...prev, progress: true }));
+          break;
+        case 5:
+          setProvApplied((prev) => ({ ...prev, done: true }));
+          setProvFinal(doneStep(msg.current.completed));
+          break;
+        default:
+          setProvApplied(doneStep(event.message!, false));
+          setProvFinal(doneStep(msg.current.applyError, true));
+      }
+    });
+
+    return function () {
+      console.log('Removed listeners');
+      eventEmitter.removeAllListeners('scanBle');
+      eventEmitter.removeAllListeners('connection');
+      eventEmitter.removeAllListeners('provisioning');
+    };
+  }, [devicePrefix]);
+
+  return {
+    bleDevices,
+    wifiAPs,
+    loading,
+    status,
+    currentStep,
+    currentWifi,
+    currentDevice,
+    results: [provSent, provApplied, provFinal],
+    setCurrentStep,
+    connectDevice,
+    configWifi,
+    doProvisioning,
+  };
+}
+
+function doProvisioning(_wifi: WifiAP | WifiAPWithPwd) {
+  RNTEspIdf.doProvisioning(
+    _wifi.ssid,
+    'password' in _wifi ? _wifi.password : ''
+  );
+}
+
+export type ProvisioningStepStatus = {
+  done: boolean;
+  progress: boolean;
+  failed: boolean;
+  message: string;
+};
+
+function initStep(message: string, progress = false): ProvisioningStepStatus {
+  return {
+    done: false,
+    progress,
+    failed: false,
+    message,
+  };
+}
+
+function doneStep(message: string, failed = false): ProvisioningStepStatus {
+  return {
+    progress: false,
+    done: true,
+    failed,
+    message,
+  };
 }
